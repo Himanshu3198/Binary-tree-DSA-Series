ec7bee56c47d450cb2f8f4368d67f5a880fec78e
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2021-03-07 13:52:12 +0530

    Add files via upload

---
+++ BinaryTree-implementation.cpp
@@ -0,0 +1,73 @@
+// simple implementation of binary tree using recursion in cpp
+// time Complexity  O(h) h:height of tree
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+};
+
+struct node *newNode(int data)
+{
+    struct node *newnode = new node();
+    newnode->data = data;
+    newnode->left = NULL;
+    newnode->right = NULL;
+    return (newnode);
+}
+
+void PreOrder(struct node *root)
+{
+
+    if (root != NULL)
+    {
+
+        cout << root->data << " ";
+        PreOrder(root->left);
+        PreOrder(root->right);
+    }
+    
+}
+
+void inOrder(struct node *root){
+
+    if(root!=NULL){
+        inOrder(root->left);
+        cout<<root->data<<" ";
+        inOrder(root->right);
+
+        
+    }
+    
+}
+
+void postOrder(struct node *root){
+    if(root!=NULL){
+    postOrder(root->left);
+    postOrder(root->right);
+    cout<<root->data<<" ";
+    }
+   
+}
+int main()
+{
+
+    struct node *root;
+    root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(4);
+
+    cout << "preorder\n";
+    PreOrder(root);
+     cout<<"\n";
+    inOrder(root);
+     cout<<"\n";
+
+    postOrder(root);
+    cout<<"\n";
+}
