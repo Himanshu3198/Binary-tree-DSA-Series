9bb98eaca62969bbf2b1abb73528ac45bb3476c3
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2021-05-05 23:12:44 +0530

    Create BinaryTree-SameTree.cpp

---
+++ BinaryTree-SameTree.cpp
@@ -0,0 +1,36 @@
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+     
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        
+        if(p==0 or q==0){
+            
+            if(p==0 and q==0){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        
+          if(p->val==q->val){
+              return isSameTree(p->left,q->left) && isSameTree(p->right ,q->right);
+          }
+           else{
+               return false;
+           }
+        
+    }
+};
