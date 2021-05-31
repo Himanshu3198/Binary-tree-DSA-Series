8c3b292044c8033b7c9f0ca2ac26740dc9c68bfe
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2021-05-31 23:47:17 +0530

    Update Binarytree-diameteroftree.cpp

--- Binarytree-diameteroftree.cpp
+++ Binarytree-diameteroftree.cpp
@@ -1,57 +1,42 @@
 // simple implementation diameter of binary tree using recursion in cpp
 // time Complexity  O(h) h:height of tree
 
-#include <bits/stdc++.h>
-using namespace std;
-
-struct node
-{
-    int data;
-    struct node *left;
-    struct node *right;
-};
-
-struct node *newNode(int data)
-{
-    struct node *newnode = new node();
-    newnode->data = data;
-    newnode->left = NULL;
-    newnode->right = NULL;
-    return (newnode);
-}
-  int ma=INT_MIN;
-int dim(struct node *root){
-    if(!root){
-        return 0;
-    }
-
-    int x=dim(root->left);
-    int y=dim(root->right);
-    ma=max(ma,x+y+1);
-
-    return max(x,y)+1;
-
-}
- int diameter(struct node * root){
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+      int maxlen=INT_MIN;
+    
+    int solve(TreeNode *root){
+        
+        
+         if(!root){
+             return 0 ;
+         }
+         
+       int left= solve(root->left);
+       int right= solve(root->right);
+        
+         maxlen=max(maxlen,left+right);
+        
+        return max(left,right) +1;
+        
        
-
-     
-       dim(root);
-
-       return ma;
-    
-    
     }
-int main()
-{
-
-    struct node *root;
-    root = newNode(1);
-    root->left = newNode(2);
-    root->right = newNode(3);
-    root->left->left = newNode(4);
-
-    cout<<diameter(root)<<"\n";
-    
-    cout<<"\n";
-}
+     
+    int diameterOfBinaryTree(TreeNode* root) {
+        
+        solve(root);
+        return maxlen;
+    }
+};
