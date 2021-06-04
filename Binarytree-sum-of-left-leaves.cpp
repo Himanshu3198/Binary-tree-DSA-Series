efa5bec1ed2b6ab84e2bf6d935f1ed16850cfa76
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2021-06-04 13:23:50 +0530

    Update Binarytree-sum-of-left-leaves.cpp

--- Binarytree-sum-of-left-leaves.cpp
+++ Binarytree-sum-of-left-leaves.cpp
@@ -1,19 +1,45 @@
 
 404. Sum of Left Leaves
 
-   int sumOfLeftLeaves(TreeNode* root) {
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    
+      void sumLeft(TreeNode *root,int &sum){
+          
+          if(!root){
+              return;
+          }
+          
+          if(root->left){
+              
+              if(root->left->left==NULL and root->left->right==NULL){
+                  sum+=root->left->val;
+              }
+          }
+          
+          sumLeft(root->left,sum);
+          sumLeft(root->right,sum);
+          
+          
+      }
+     int sumOfLeftLeaves(TreeNode* root) {
         if(!root)
             return 0;
        int sum =0;
         sumLeft(root,sum);
         return sum;
     }
-    void sumLeft(TreeNode* root, int& sum) {
-        if(!root)
-            return;
-        if(root->left && root->left->left == NULL && root->left->right == NULL){
-            sum+=root->left->val;
-        }
-        sumLeft(root->right,sum);
-        sumLeft(root->left,sum);
-    }
+ 
+};
