2c25e2c6c447e2793ae0d1cf25948637ee183536
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2022-03-09 00:03:26 +0530

    Update bfs-662. Maximum Width of Binary Tree.cpp

--- bfs-662. Maximum Width of Binary Tree.cpp
+++ bfs-662. Maximum Width of Binary Tree.cpp
@@ -4,59 +4,57 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode() : val(0), left(nullptr), righ t(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
+    
+//        time complexity O(n)
+//        space complexity O(no of nodes at particular level)
     int widthOfBinaryTree(TreeNode* root) {
         
         
-           int maxWidth=0;
+           long long int maxWidth=0;
+          queue<pair<TreeNode *,long long int>>q;
         
-          queue<pair<TreeNode *,int>>q;
+          q.push({root,0});
         
-        q.push({root,0});
-        
-        
-        while(!q.empty()){
-            
-            
-            int minNode=q.front().second;
-            int size=q.size();
-            
-            int first,last;
-            
-            for(int i=0;i<size;i++){
-                
-                TreeNode *curr=q.front().first;
-                 int currIdx=q.front().second-minNode;
-                  q.pop();
-                
-                 if(i==0){
-                     first=currIdx;
-                 }
-                   if(i==size-1){
-                     last=currIdx;
-                 }
-                
-                if(curr->left){
-                    q.push({curr->left,currIdx*2+1});   
-                }
-                           
-                           
-                if(curr->right){
-                    q.push({curr->right,currIdx*2+2});
-                }
-                          
-                        
+          while(!q.empty()){
+              
+              
+              
+                int size=q.size();
+                long long int convert=q.front().second;
+                  long long int first,last;
+              for(long long int i=0;i<size;i++){
+                  
+                   auto [node,idx]=q.front();
+                      q.pop();
+                  idx=idx-convert;
+                   if(i==0){
+                       
+                       first=idx;
+                   }if(i==size-1){
+                       last=idx;
+                   }
+                  
+                  if(node->left){
+                      q.push({node->left,2LL*idx+1});
+                  }
+                  if(node->right){
+                      
+                      q.push({node->right,2LL*idx+2});
+                  }
+                  
+                  maxWidth=max(maxWidth,last-first+1);
               }
-                           
-                maxWidth=max(maxWidth,last-first+1);
           }
-       
-               return maxWidth;
+        
+        return maxWidth;
+        
+      
         }
 };
